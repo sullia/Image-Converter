c7f26f1a1e448b99fec953b1fbab7aeaa43adff8
Adrian O'Sullivan <48090655+sullia@users.noreply.github.com>
2020-06-05 16:10:00 +0100

    Adding convert file

---
+++ convert.ps1
@@ -0,0 +1,27 @@
+function ConvertTo-Jpg
+{
+    [cmdletbinding()]
+    param([Parameter(Mandatory=$true, ValueFromPipeline = $true)] $Path)
+
+    process{
+        if ($Path -is [string])
+        {$Path = get-childitem $Path}
+
+        If (!(test-path './bin'))
+        {New-Item -ItemType Directory -Path './bin'}
+
+        $Path | ForEach-Object {
+            $image = [System.Drawing.Image]::FromFile($($_.FullName));
+            $Output = [IO.Path]::Combine($_.Directory, 'bin', $_.Name);
+            $FilePath = [IO.Path]::ChangeExtension($Output, '.png');
+            $image.Save($FilePath, [System.Drawing.Imaging.ImageFormat]::Png);
+            $image.Dispose();
+        }
+    }
+
+}
+
+Set-Location .\bmp
+
+Get-ChildItem *.bmp | ConvertTo-Jpg
+cd..
